9a500acb6d5a6ddfd56eb047e40519ab767bc443
Aneesh-Peri <aneesh.peri@gmail.com>
2024-06-16 23:43:30 -0400

    finished function

--- Implementing Data Structures in Python/Stack/Undo Feature/undo.py
+++ Implementing Data Structures in Python/Stack/Undo Feature/undo.py
@@ -1,16 +1,29 @@
 def undo(str):
-    lst = []
+    lst = list(str)
     stack = []
-    for i in str:
-        lst.append(i)
-    print(lst)
-    numChar = int(input("Enter the number of letters you would like to remove: "))
-    i = 0
-    print(lst.pop() + " popped later")
     # Work on this part later
-    while i < numChar:
-        stack.append(i)
-    print(stack)
+    try:
+        num_char = int(input("Enter the number of letters you would like to remove: "))
+        if num_char > len(lst):
+            raise ValueError("Number of characters to remove exceeds the string length.")
+    except ValueError as ve:
+        print(f"Invalid input: {ve}")
+        return
+
+    # Remove the specified number of characters and push them onto the stack
+    for _ in range(num_char):
+        if lst:
+            char = lst.pop()
+            stack.append(char)
+        else:
+            break
+
+    print(f"Modified list after undo operation: {lst}")
+    print(f"Stack with removed characters: {stack}")
+
+    # Reconstruct the modified string
+    modified_text = ''.join(lst)
+    print(modified_text)
     
 
 undo("Hello")
