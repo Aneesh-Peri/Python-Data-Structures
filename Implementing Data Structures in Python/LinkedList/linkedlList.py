e241c8c70972ac3f7500be777a35298154f4920c
Aneesh-Peri <aneesh.peri@gmail.com>
2024-06-19 15:30:38 -0400

    2nd commit about linkedlists

--- Implementing Data Structures in Python/LinkedList/linkedlList.py
+++ Implementing Data Structures in Python/LinkedList/linkedlList.py
@@ -84,8 +84,46 @@ class LinkedList:
             itr = itr.next
             count += 1
 
+    def insert_after_value(self, data_after, data_to_insert):
+
+        if self.head is None:
+            return
+
+        if self.head == data_after:
+            self.head.next = Node(data_to_insert, self.head.next)
+        itr = self.head
+        while itr.data != data_after:
+            itr = itr.next
+        self.head.next = Node(data_to_insert, self.head.next)
+
+    def remove_by_value(self, data):
+        if self.head is None:
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        itr = self.head
+        while itr.next:
+            if itr.next.data == data:
+                itr.next = itr.next.next
+                return
+            itr = itr.next
+
 
 if __name__ == "__main__":
     ll = LinkedList()
     ll.insert_values(["banana", "mango", "grapes", "orange"])
+    ll.print()
+    ll.insert_after_value("mango", "apple")
+    ll.print()
+    ll.remove_by_value("orange")
+    ll.print()
+    ll.remove_by_value("figs")
+    ll.print()
+    ll.remove_by_value("banana")
+    ll.remove_by_value("mango")
+    ll.remove_by_value("apple")
+    ll.remove_by_value("grapes")
     ll.print()
